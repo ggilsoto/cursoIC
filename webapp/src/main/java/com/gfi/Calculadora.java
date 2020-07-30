b8dd28f249aa72b092b45beab313ee48a8e3900b
Ggilsoto <ggilsoto@gmail.com>
2020-07-30 11:36:49 +0000

    jacoco

--- webapp/src/main/java/com/gfi/Calculadora.java
+++ webapp/src/main/java/com/gfi/Calculadora.java
@@ -2,6 +2,10 @@ package com.gfi;
 
 public class Calculadora 
 {
+    /*
+    * Esta función suma 2 numeros
+    * @return El resultado de la suma
+    */
     public static int suma( int numero1, int numero2 )
     {
         return numero1+numero2;
